930494fffd54ef840777514308d511f9ff6ba569
Bogterr <94174009+Bogterr@users.noreply.github.com>
2024-11-12 04:23:28 +0700

    Add files via upload

---
+++ Module_9_6.py
@@ -0,0 +1,30 @@
+# Генераторы
+
+
+def all_variants(text):
+    len_txt = len(text)
+    list = []
+    for run in range(len_txt):
+        for stop in range(run + 1, len_txt + 1):
+            list.append(text[run:stop])
+
+    qsort(list)
+    return list
+
+
+def qsort(a):
+    stop = False
+    while stop == False:
+        count = 0
+        for i in range(len(a) - 1):
+            if len(a[i]) > len(a[i + 1]):
+                a[i], a[i + 1] = a[i + 1], a[i]
+                count += 1
+            elif count == 0 and i == len(a) - 2:
+                stop = True
+    return a
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
